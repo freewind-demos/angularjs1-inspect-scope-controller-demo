bd0115f2b3452000812a7c5d69e89ade22e3e176
freewind <nowindlee@gmail.com>
2019-03-08 19:30:50 +0800

    demo

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const app = angular.module('app', []);
 app.controller('HelloController', function ($scope) {
-    $scope.userName = "AngularJs1";
-    $scope.showHelloWords = false;
-    $scope.sayHello = function () {
-        this.showHelloWords = true
-    }
+  $scope.userName = "AngularJs1";
+  $scope.inspectMessage = '';
+  $scope.inspectInjector = function () {
+    const injector = angular.element(document.querySelector('body')).injector();
+    $scope.inspectMessage = injector.get('$rootScope');
+  }
 });
